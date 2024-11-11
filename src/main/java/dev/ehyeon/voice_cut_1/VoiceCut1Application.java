136d8d91b7157187d614fcd6d83bd6f9775e6778
49EHyeon42 <49ehyeon42@gmail.com>
2024-11-11 19:16:20 +0900

    init: 프로젝트 시작

---
+++ src/main/java/dev/ehyeon/voice_cut_1/VoiceCut1Application.java
@@ -0,0 +1,12 @@
+package dev.ehyeon.voice_cut_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceCut1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceCut1Application.class, args);
+	}
+}
